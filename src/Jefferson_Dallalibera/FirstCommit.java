9c055ea8d2a0e34edef2d694d1c69d023b5ac833
JEFFERSON MIGUEL DALLALIBERA <jeffersonmigueldallalibera@gmail.com>
2022-08-10 20:16:58 -0300

    First Commit Jefferson

---
+++ src/Jefferson_Dallalibera/FirstCommit.java
@@ -0,0 +1,13 @@
+package Jefferson_Dallalibera;
+
+public class FirstCommit {
+
+         public  static  void main(String ars[]){
+
+             /* classe String */
+             String myFirstString = "First Commit using java";
+             System.out.println(myFirstString);
+
+         }
+
+}
