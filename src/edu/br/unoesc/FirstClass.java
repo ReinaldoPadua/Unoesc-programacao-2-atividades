8b822b0b2c25bccc98e2560bf601baf1b33cf1e2
reinaldo.padua@unoesc.edu.br <Punk19862028!>
2022-08-09 21:57:10 -0300

    Inicializando repositório para atividades em sala

---
+++ src/edu/br/unoesc/FirstClass.java
@@ -0,0 +1,17 @@
+package edu.br.unoesc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FirstClass {
+
+
+    public  static  void main(String ars[]){
+
+        /* classe String */
+        String myFirstString = "Hello peoples";
+        System.out.println(myFirstString);
+
+    }
+
+}
