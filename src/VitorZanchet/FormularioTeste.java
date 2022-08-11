881f54dab026074f4f539e683889d7a69772d305
Vitor Augusto Bordignon Zanchet <vitor.augusto.bordignon@hotmail.com>
2022-08-10 22:04:54 -0300

    Vitor-Zanchet

---
+++ src/VitorZanchet/FormularioTeste.java
@@ -0,0 +1,28 @@
+package VitorZanchet;
+import java.util.Scanner;
+
+public class FormularioTeste {
+
+    public static void main(String ars[]){
+        boolean boss = true;
+
+        while (boss);{
+            Scanner teclado = new Scanner(System.in);
+            System.out.println("Entre com o seu nome: ");
+            String nome = teclado.nextLine();
+            boss = false;
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
