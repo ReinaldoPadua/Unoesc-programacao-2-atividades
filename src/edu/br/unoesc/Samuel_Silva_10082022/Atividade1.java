5f4d1d133930b309c55ea443278460d02967f4eb
Samuelsyspro <samuel.silva@unoes.edu.br>
2022-08-13 10:54:34 -0300

    Classe atualizada e com os prints

--- src/edu/br/unoesc/Samuel_Silva_10082022/Atividade1.java
+++ src/edu/br/unoesc/Samuel_Silva_10082022/Atividade1.java
@@ -1,34 +1,57 @@
 package edu.br.unoesc.Samuel_Silva_10082022;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Atividade1 {
+    public Atividade1() {
+    }
+
     public static void main(String[] args) {
         System.out.println("atividade 1");
         Scanner leitor = new Scanner(System.in);
+        System.out.println("Digite o nome do colaborador: ");
         String nome = leitor.nextLine();
+        System.out.println("Digite o cpf do colaborador: ");
         String cpf = leitor.nextLine();
+        System.out.println("Digite a data de nascimento do colaborador: ");
         Integer data_nascimento = leitor.nextInt();
+        leitor.nextLine();
+        System.out.println("Digite o endereço do colaborador: ");
         String endereco = leitor.nextLine();
+        System.out.println("Digite o salario do colaborador: ");
         Double salario = leitor.nextDouble();
+        leitor.nextLine();
+        System.out.println("Digite a departamento do colaborador: ");
         String departamento = leitor.nextLine();
+        System.out.println("Digite a função do colaborador: ");
         String funcao = leitor.nextLine();
+        System.out.println("Digite a quntidade de certificações do colaborador: ");
         Integer numero_certificacoes = leitor.nextInt();
-        String nome_certificacoes = leitor.nextLine();
+        leitor.nextLine();
 
-        // prints dos inputs do colaborador
-        System.out.println("Nome colaborador" +nome);
-        System.out.println("CPF colaborador" +cpf);
-        System.out.println("Data nascimento colaborador" +data_nascimento);
-        //calculo da idade do colaborador atual
-        System.out.println("Endereco colaborador" +endereco);
-        System.out.println("Salario colaborador" +salario);
-        System.out.println("Departamento colaborador" +departamento);
-        System.out.println("Funcao colaborador" +funcao);
-        System.out.println("Numero certificacoes colaborador" +numero_certificacoes);
-        System.out.println("Nome certificacoes colaborador" +nome_certificacoes);
+        List<String>  certificacoes = new  ArrayList<String>();
+        while(numero_certificacoes>0) {
+            System.out.println("Digite os nomes das certificações do colaborador: ");
+            certificacoes.add(leitor.nextLine());
+            numero_certificacoes--;
+        }
 
+        System.out.println("Nome colaborador: " + nome);
+        System.out.println("CPF colaborador: " + cpf);
+        System.out.println("Data nascimento colaborador: " + data_nascimento);
+        LocalDate data_atual = LocalDate.now();
+        Integer idade = data_atual.getYear() - data_nascimento;
+        System.out.println("Idade colaborador: " + idade);
+        System.out.println("Endereco : " + endereco);
+        System.out.println("Salario : " + salario);
+        System.out.println("Salario bruto anual : " + salario * 12);
+        System.out.println("Departamento do colaborador: " + departamento);
+        System.out.println("Funcao colaborador: " + funcao);
+        System.out.println("Numero certificacoes: " + numero_certificacoes);
+        System.out.println("Nome certificaçôes:" + certificacoes);
 
 
     }
