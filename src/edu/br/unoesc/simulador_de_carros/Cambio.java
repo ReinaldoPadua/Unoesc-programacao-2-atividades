b3e97b51cbd8009fc600ee203dedc81e89ba0f0d
Eduardo Luis Mortari <110139782+Manorizer@users.noreply.github.com>
2022-08-24 17:57:46 -0300

    Update Cambio.java

--- src/edu/br/unoesc/simulador_de_carros/Cambio.java
+++ src/edu/br/unoesc/simulador_de_carros/Cambio.java
@@ -1,34 +1,39 @@
-package edu.br.unoesc.simulador_de_carros;
+package Eduardo_Mortari_17082022;
 
 public class Cambio {
-
     private Boolean automatico;
 
-    private Integer numeroMarchas;
+    private Integer numerodeMarchas;
 
     private Integer marchaAtual;
 
-    public Cambio(Boolean automatico,Integer numeroMarchas){
+    public Cambio(Boolean automatico, Integer numerodeMarchas) {
         super();
         this.automatico = automatico;
-        this.numeroMarchas = numeroMarchas;
+        this.numerodeMarchas = numerodeMarchas;
         this.marchaAtual = 0;
     }
 
-    public Boolean subirMarcha(){
+
+
+    public Boolean subirMarcha() {
+        if (this.marchaAtual < this.numerodeMarchas) {
+            this.marchaAtual++;
+            return true;
+        }
         return false;
     }
 
-    public Boolean reduzirMarcha(){
-        if(this.marchaAtual>0){
+    public Boolean reduzirMarcha() {
+        if (this.marchaAtual > 0) {
             this.marchaAtual--;
             return true;
         }
         return false;
     }
 
-    public Boolean engatarMarchaRe(){
-        if(this.marchaAtual<=this.numeroMarchas){
+    public Boolean engatarMarchaRe() {
+        if (this.marchaAtual <= this.numerodeMarchas) {
             this.marchaAtual++;
             return true;
         }
@@ -36,13 +41,12 @@ public class Cambio {
         return false;
     }
 
-    public Integer getNumeroMarchas() {
-        return this.numeroMarchas;
+    public Integer getNumerodeMarchas() {
+        return this.numerodeMarchas;
     }
 
     public Integer getMarchaAtual() {
         return this.marchaAtual;
     }
-
 
 }
